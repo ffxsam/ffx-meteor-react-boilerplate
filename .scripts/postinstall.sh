42f5843e7c73575aa8be029f113d3e1bac8adaed
Sam <ffxsam@users.noreply.github.com>
2016-01-30 01:51:10 -0800

    Cleanup on example app

--- .scripts/postinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo
-echo "NPM packages installed. Updating Meteor packages...\c"
-meteor update >/dev/null
-echo "done!"
--- .scripts/scaffold.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-git checkout -f master
-rm -rf .git README.md
