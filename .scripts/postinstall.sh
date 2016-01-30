fe043e83b9ce5d5fff27d9fbe22213c00a96e908
Sam <ffxsam@users.noreply.github.com>
2016-01-30 02:06:38 -0800

    More changes

--- .scripts/postinstall.sh
+++ .scripts/postinstall.sh
@@ -4,3 +4,5 @@ echo "NPM packages installed. Updating Meteor packages...\c"
 meteor update >/dev/null
 echo "done!"
 echo
+echo "Don't forget to do 'npm run scaffold'."
+echo
--- .scripts/scaffold.sh
+++ .scripts/scaffold.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
-git checkout -f master
+echo "Cleaning up unneeded files and creating folder structure...\c"
+echo "Doing the following:"
+echo
+echo "1. Removing unnecessary files/folders"
+echo "2. Creating empty folder structure (lib/collections, etc)"
+echo "3. Rewriting package.json"
+
 rm -rf .git README.md
 
 # Set up folder structure
 mkdir -p lib/collections
+
+cat package.json | sed '5,10d' | sed "s/ffx-meteor-react-boilerplate/$USER-project/" | sed 's/1.0.0/0.1.0/' | sed "s/ffxsam/$USER/" | sed 's/Meteor 1.3 + React boilerplate/Description here/' > package.new.json
+mv package.new.json package.json
+
+echo
+echo "Done!"
+echo
