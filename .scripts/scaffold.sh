d57badbc4e19b3586b5f63c3b5d128e6ef56a302
Sam <ffxsam@users.noreply.github.com>
2016-01-30 13:45:32 -0800

    Extra folders in client/

--- .scripts/scaffold.sh
+++ .scripts/scaffold.sh
@@ -8,7 +8,7 @@ echo "3. Rewriting package.json"
 rm -rf .git README.md
 
 # Set up folder structure
-mkdir -p lib/collections
+mkdir -p lib/collections client/containers client/components
 
 cat package.json | sed '5,11d' | sed "s/ffx-meteor-react-boilerplate/$USER-project/" | sed 's/1.0.0/0.1.0/' | sed "s/ffxsam/$USER/" | sed 's/Meteor 1.3 + React boilerplate/Description here/' > package.new.json
 mv package.new.json package.json
