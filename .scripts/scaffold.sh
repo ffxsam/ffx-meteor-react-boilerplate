e0ad1464cf805d4d20b2dc7b9f39748bd00dda6e
Sam <ffxsam@users.noreply.github.com>
2016-02-05 20:07:53 -0800

    Oops, fixed a bug in scaffold script

--- .scripts/scaffold.sh
+++ .scripts/scaffold.sh
@@ -16,7 +16,8 @@ mkdir -p client/components features/SomeFeature/client/components \
   features/SomeFeature/server
 
 cat package.json | sed '5,11d' | \
-  sed "s/ffx-meteor-react-boilerplate/$USER-project/" | sed 's/1.0.0/0.1.0/' | \
+  sed "s/ffx-meteor-react-boilerplate/$USER-project/" | \
+  sed 's/"version": "1.0.0"/"version": "0.1.0"/' | \
   sed "s/ffxsam/$USER/" | \
   sed 's/Meteor 1.3 + React boilerplate/Description here/' > package.new.json
 mv package.new.json package.json
