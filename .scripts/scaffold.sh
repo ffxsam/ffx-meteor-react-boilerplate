c47640a5855d82bbf95f9bb4081fe8d767476c41
Sam <ffxsam@users.noreply.github.com>
2016-02-05 18:07:07 -0800

    Minor changes to scaffold process

--- .scripts/scaffold.sh
+++ .scripts/scaffold.sh
@@ -2,15 +2,20 @@
 echo "Doing the following:"
 echo
 echo "1. Removing unnecessary files/folders"
-echo "2. Creating empty folder structure (lib/collections, etc)"
-echo "3. Rewriting package.json"
+echo "2. meteor reset (wipes out .meteor/local)"
+echo "3. Creating empty folder structure (lib/collections, etc)"
+echo "4. Rewriting package.json"
 
 rm -rf .git README.md
+meteor reset
 
 # Set up folder structure
 mkdir -p lib/collections client/containers client/components
 
-cat package.json | sed '5,11d' | sed "s/ffx-meteor-react-boilerplate/$USER-project/" | sed 's/1.0.0/0.1.0/' | sed "s/ffxsam/$USER/" | sed 's/Meteor 1.3 + React boilerplate/Description here/' > package.new.json
+cat package.json | sed '5,11d' | \
+  sed "s/ffx-meteor-react-boilerplate/$USER-project/" | sed 's/1.0.0/0.1.0/' | \
+  sed "s/ffxsam/$USER/" | \
+  sed 's/Meteor 1.3 + React boilerplate/Description here/' > package.new.json
 mv package.new.json package.json
 
 echo
