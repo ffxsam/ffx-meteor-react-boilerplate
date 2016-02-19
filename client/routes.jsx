945297b73faebb153f371e86f4ed9b20ebfd5223
Sam <ffxsam@users.noreply.github.com>
2016-02-18 21:57:38 -0800

    More explicit with imports

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {FlowRouter} from 'meteor/kadira:flow-router';
 import {mount} from 'react-mounter';
+import MainLayout from './layouts/MainLayout.jsx';
+import Home from './pages/Home.jsx';
 
 FlowRouter.route('/', {
   name: 'home',
