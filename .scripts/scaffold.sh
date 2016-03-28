af1f98a6054c9cd645ba93833b71e1c3776d3fa2
Sam <ffxsam@users.noreply.github.com>
2016-03-28 14:28:04 -0700

    Restructuring stuff a bit better
    Also added Enzyme testing

--- .scripts/scaffold.sh
+++ .scripts/scaffold.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 echo "Doing the following:"
 echo
-echo "1. Removing unnecessary files/folders"
-echo "2. meteor reset (wipes out .meteor/local)"
-echo "3. Creating empty folder structure (lib/collections, etc)"
-echo "4. Rewriting package.json"
+echo "1. Rewriting package.json"
+echo "2. Removing unnecessary files/folders"
+echo "3. meteor reset (wipes out .meteor/local)"
+echo "4. Creating empty folder structure (lib/collections, etc)"
 echo
+
+cp .scripts/package-dist.json package.json
 
 rm -rf .git README.md .scripts
 meteor reset
@@ -14,15 +16,9 @@ meteor reset
 mkdir -p client/components \
   features/SomeFeature/client/components \
   features/SomeFeature/client/containers \
+  features/SomeFeature/lib \
   features/SomeFeature/server
 touch server/publications.js
-
-cat package.json | sed '5,11d' | \
-  sed "s/ffx-meteor-react-boilerplate/$USER-project/" | \
-  sed 's/"version": "1.0.0"/"version": "0.1.0"/' | \
-  sed "s/ffxsam/$USER/" | \
-  sed 's/Meteor 1.3 + React boilerplate/Description here/' > package.new.json
-mv package.new.json package.json
 
 echo
 echo "Done!"
