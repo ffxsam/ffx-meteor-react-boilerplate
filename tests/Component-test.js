de262154d7a5af96ce563ec4f068f67b0bd20840
Sam <ffxsam@users.noreply.github.com>
2016-05-17 10:54:36 -0700

    Added Enzyme test boilerplate code

---
+++ tests/Component-test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+//import Component from '../client/components/Component';
+
+describe('<Component />', _ => {
+  xit('does something', done => {
+    done();
+  });
+});
