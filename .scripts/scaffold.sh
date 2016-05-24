7e60a4aba96b6599b269c87ac40a14a6eda0939e
Sam <ffxsam@users.noreply.github.com>
2016-05-24 13:13:27 -0700

    Wrap-up

--- .scripts/scaffold.sh
+++ .scripts/scaffold.sh
@@ -4,7 +4,6 @@ echo
 echo "1. Rewriting package.json"
 echo "2. Removing unnecessary files/folders"
 echo "3. meteor reset (wipes out .meteor/local)"
-echo "4. Creating empty folder structure (lib/collections, etc)"
 echo
 
 cp .scripts/package-dist.json package.json
