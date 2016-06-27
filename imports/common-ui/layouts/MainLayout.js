a15ff41b46f370842ac8cac2301de7532eceea65
Sam <ffxsam@users.noreply.github.com>
2016-06-27 00:24:04 -0700

    Removed leftover tap event injection

--- imports/common-ui/layouts/MainLayout.js
+++ imports/common-ui/layouts/MainLayout.js
@@ -1,9 +1,6 @@
 import React, { PropTypes } from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Provider } from 'react-redux';
 import store from '/imports/redux/store';
-
-injectTapEventPlugin();
 
 export default MainLayout = ({ content }) => {
   return <Provider store={store}>
